bca53dd9ac48958f122fff3ddd77ee5c1d7727a3
custom82 <andrea.postiglione@gmail.com>
2025-12-06 15:28:03 +0100

    .

--- include/Commitment.h
+++ include/Commitment.h
@@ -1,37 +1,38 @@
 #ifndef COMMITMENT_H
 #define COMMITMENT_H
 
-#include "bignum.h"
-#include "Stream.h"
-#include "CommitmentProofOfKnowledge.h"
-#include "Accumulator.h"
-#include "Zerocoin.h"
+#include "zerocoin_defs.h"
 
 namespace libzerocoin {
 
 	class Commitment {
-	public:
-		Commitment(const IntegerGroupParams* p, const CBigNum& value);
-		void Serialize(Stream& s) const;
-		void Unserialize(Stream& s);
-
 	private:
 		const IntegerGroupParams* params;
-		CBigNum value;
+		CBigNum contents;
+
+	public:
+		Commitment(const IntegerGroupParams* p, const CBigNum& value);
+
+		const CBigNum& getContents() const { return contents; }
+		const IntegerGroupParams* getParams() const { return params; }
 	};
 
 	class CommitmentProofOfKnowledge {
-	public:
-		CommitmentProofOfKnowledge(const IntegerGroupParams* params, const Commitment& commitment);
-		void Serialize(Stream& s) const;
-		void Unserialize(Stream& s);
-
 	private:
-		Commitment commitment;
-		Bignum C_e;
-		Bignum C_u;
-		Bignum C_r;
-	};
-}
+		CBigNum C_e;
+		CBigNum C_u;
+		CBigNum C_r;
 
-#endif // COMMITMENT_H
+	public:
+		CommitmentProofOfKnowledge() {}
+		CommitmentProofOfKnowledge(const IntegerGroupParams* params, const Commitment& commitment);
+
+		// Getters
+		const CBigNum& getC_e() const { return C_e; }
+		const CBigNum& getC_u() const { return C_u; }
+		const CBigNum& getC_r() const { return C_r; }
+	};
+
+} // namespace libzerocoin
+
+#endif
